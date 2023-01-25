1523d1b660a615ffdaad253c219ed302715695de
Hnes-co <60889935+Hnes-co@users.noreply.github.com>
2023-01-25 19:59:10 +0200

    created endpoints to GET journeys & stations
    + added endpoint for journey list pagination

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { importCsvToDatabase } from './assets/databaseImport';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import Journey from './models/journey';
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,6 +23,46 @@ app.post('/api/import', async (req, res) => {
   const paths: string[] = req.body.paths;
   const result = await importCsvToDatabase(paths);
   res.json(result);
+});
+
+app.get('/api/journeys', async (req, res) => {
+  console.log('Method:', req.method);
+  console.log('Path:  ', req.path);
+  try {
+    const journeys = await Journey.find({}, null, { limit: 1000 });
+    const size = await Journey.countDocuments();
+    res.send({ data: journeys, size: size });
+  }
+  catch(error) {
+    res.status(500).json(error);
+  }
+});
+
+app.get('/api/journeys/:index', async (req, res) => {
+  console.log('Method:', req.method);
+  console.log('Path:  ', req.path);
+  const skip = Number(req.params.index ?? 0);
+  try {
+    const journeys = await Journey.find({}, null, { limit: 1000, skip: skip });
+    res.send({ data: journeys });
+  }
+  catch(error) {
+    res.status(500).json(error);
+  }
+});
+
+app.get('/api/stations', async (req, res) => {
+  console.log('Method:', req.method);
+  console.log('Path:  ', req.path);
+  try {
+    const stations = await Journey.distinct("departureStationName");
+    //const returnStations = await Journey.distinct("returnStationName");
+    //const uniqueStations = stations.concat(returnStations.filter(returnStation => !stations.includes(returnStation)));
+    res.send({ data: stations, size: stations.length });
+  }
+  catch(error) {
+    res.status(500).json(error);
+  }
 });
 
 app.listen(PORT, () => {
